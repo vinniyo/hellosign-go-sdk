4475901ca7ffece619679d0a2692e58125bdf1a5
Joe Heth <joeheth@gmail.com>
2022-04-26 14:59:58 -0400

    uncomment AllowDecline and AllowReassign

--- hellosign.go
+++ hellosign.go
@@ -28,23 +28,23 @@ type Client struct {
 
 // CreationRequest contains the request parameters for create_embedded
 type CreationRequest struct {
-	TestMode           bool              `form_field:"test_mode"`
-	ClientID           string            `form_field:"client_id"`
-	FileURL            []string          `form_field:"file_url"`
-	File               []string          `form_field:"file"`
-	Title              string            `form_field:"title"`
-	Subject            string            `form_field:"subject"`
-	Message            string            `form_field:"message"`
-	SigningRedirectURL string            `form_field:"signing_redirect_url"`
-	Signers            []Signer          `form_field:"signers"`
-	Attachments        []Attachment      `form_field:"attachments"`
-	CustomFields       []CustomField     `form_field:"custom_fields"`
-	CCEmailAddresses   []string          `form_field:"cc_email_addresses"`
-	UseTextTags        bool              `form_field:"use_text_tags"`
-	HideTextTags       bool              `form_field:"hide_text_tags"`
-	Metadata           map[string]string `form_field:"metadata"`
-	// AllowDecline          int                   `form_field:"allow_decline"`
-	// AllowReassign         int                   `form_field:"allow_reassign"`
+	TestMode              bool                  `form_field:"test_mode"`
+	ClientID              string                `form_field:"client_id"`
+	FileURL               []string              `form_field:"file_url"`
+	File                  []string              `form_field:"file"`
+	Title                 string                `form_field:"title"`
+	Subject               string                `form_field:"subject"`
+	Message               string                `form_field:"message"`
+	SigningRedirectURL    string                `form_field:"signing_redirect_url"`
+	Signers               []Signer              `form_field:"signers"`
+	Attachments           []Attachment          `form_field:"attachments"`
+	CustomFields          []CustomField         `form_field:"custom_fields"`
+	CCEmailAddresses      []string              `form_field:"cc_email_addresses"`
+	UseTextTags           bool                  `form_field:"use_text_tags"`
+	HideTextTags          bool                  `form_field:"hide_text_tags"`
+	Metadata              map[string]string     `form_field:"metadata"`
+	AllowDecline          bool                  `form_field:"allow_decline"`
+	AllowReassign         bool                  `form_field:"allow_reassign"`
 	FormFieldsPerDocument [][]DocumentFormField `form_field:"form_fields_per_document"`
 	// FieldOptions map[string]string `form_field:"field_options"``
 }
